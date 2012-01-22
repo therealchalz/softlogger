fc741fc0857051f9a78eedff06190fc4235a3c1f
Charles Hache <chache@tsco.ca>
2012-01-21 16:08:58 -0800

    Adding logging lib

--- src/ca/brood/Softlogger.java
+++ src/ca/brood/Softlogger.java
@@ -1,13 +1,23 @@
 package ca.brood;
+import org.apache.commons.logging.*;
 
 public class Softlogger {
-
+	private Log log;
+	public Softlogger() {
+		
+	}
+	public void run() {
+		log = LogFactory.getLog(Softlogger.class);
+		log.fatal("Test");
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Starting...");
+		Softlogger s = new Softlogger();
+		s.run();
 	}
 
 }
