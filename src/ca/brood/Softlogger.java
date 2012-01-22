3601828cdc5f2a1f495fad35ec5dad83168d4dc1
Charles Hache <chache@tsco.ca>
2012-01-21 16:56:47 -0800

    Changing to Log4J lib

--- src/ca/brood/Softlogger.java
+++ src/ca/brood/Softlogger.java
@@ -1,14 +1,12 @@
 package ca.brood;
-import org.apache.commons.logging.*;
 
 public class Softlogger {
-	private Log log;
+
 	public Softlogger() {
 		
 	}
 	public void run() {
-		log = LogFactory.getLog(Softlogger.class);
-		log.fatal("Test");
+
 	}
 	/**
 	 * @param args
