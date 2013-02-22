3d44ded1a6ab51d7b2eae7afa3c2f1eba182ca79
Charles Hache <chache@brood.ca>
2013-02-21 19:31:23 -0800

    Releasing under the terms of the GPLv3

--- src/ca/brood/softlogger/util/XmlConfigurable.java
+++ src/ca/brood/softlogger/util/XmlConfigurable.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Charles Hache. All rights reserved. 
+ * 
+ * This file is part of the softlogger project.
+ * softlogger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * softlogger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with softlogger.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Charles Hache - initial API and implementation
+ ******************************************************************************/
 package ca.brood.softlogger.util;
 
 import org.w3c.dom.Node;
